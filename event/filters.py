31a33d8f00938e7db54b8d376aec3ae5dc42506b
Szabson <szymonzaba75@gmail.com>
2026-01-21 00:03:28 +0100

    new filter

--- event/filters.py
+++ event/filters.py
@@ -1,15 +1,27 @@
 import django_filters
 from .models import Event
 from decimal import Decimal
+from django.db.models import Q
 
 class EventListFilter(django_filters.FilterSet):
+    search = django_filters.CharFilter(method='filter_search')
     is_free = django_filters.BooleanFilter(method='filter_is_free')
+
+    def filter_search(self, queryset, name, value):
+        words = value.split()
+
+        q = Q()
+        for word in words:
+            q |= Q(title__icontains=word)
+            q |= Q(category__name__icontains=word)
+
+        return queryset.filter(q).distinct()
 
     def filter_is_free(self, queryset, name, value):
         if value:
             return queryset.filter(additional_info__price=0)
         return queryset.exclude(additional_info__price=0)
-    
+
     class Meta:
         model = Event
-        fields = ['is_free', 'title']
+        fields = ['search', 'is_free']

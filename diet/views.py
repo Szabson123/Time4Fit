56f53f99f0aaa5e6153323263922db92be1264a2
Szabson <szymonzaba75@gmail.com>
2026-08-23 20:46:05 +0200

    trial new pagin

--- diet/views.py
+++ diet/views.py
@@ -6,10 +6,10 @@ from django.db.models.functions import Coalesce
 from django.contrib.postgres.aggregates import ArrayAgg
 from django.shortcuts import get_object_or_404
 
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView, ListAPIView, RetrieveAPIView
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, CursorPagination
 from rest_framework import status
 
 from django.db import transaction
@@ -27,60 +27,9 @@ from .models import DailyMealCalendar, MealCategory, FullMeal, MealItem, Product
 from datetime import datetime
 
 
-class CustomPagination(PageNumberPagination):
+class ProductCursorPagination(CursorPagination):
     page_size = 20
-    max_page_size = 60
-
-
-class FastPageNumberPagination(PageNumberPagination):
-    page_size = 20
-    page_query_param = 'page'
-
-    def paginate_queryset(self, queryset, request, view=None):
-        self.request = request
-        self.page_size = self.get_page_size(request)
-
-        try:
-            page_number = int(request.query_params.get(self.page_query_param, 1))
-            if page_number < 1:
-                page_number = 1
-        except (TypeError, ValueError):
-            page_number = 1
-
-        self.page_number = page_number
-        offset = (page_number - 1) * self.page_size
-
-        results = list(queryset[offset : offset + self.page_size + 1])
-        
-        self.has_next = len(results) > self.page_size
-        self.has_previous = page_number > 1
-
-        return results[:self.page_size]
-
-    def get_next_link(self):
-        if not self.has_next:
-            return None
-        url = self.request.build_absolute_uri()
-        page_number = self.page_number + 1
-        from rest_framework.utils.urls import replace_query_param
-        return replace_query_param(url, self.page_query_param, page_number)
-
-    def get_previous_link(self):
-        if not self.has_previous:
-            return None
-        url = self.request.build_absolute_uri()
-        page_number = self.page_number - 1
-        from rest_framework.utils.urls import replace_query_param, remove_query_param
-        if page_number == 1:
-            return remove_query_param(url, self.page_query_param)
-        return replace_query_param(url, self.page_query_param, page_number)
-
-    def get_paginated_response(self, data):
-        return Response({
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link(),
-            'results': data
-        })
+    ordering = ('title', 'id')
 
 
 class DailyMealCalendarDetailView(GenericAPIView):
@@ -268,8 +217,8 @@ class CreateCustomMealView(GenericAPIView):
 
 class ProductListView(ListAPIView):
     serializer_class = ProductListSerializer
-    permission_classes = [IsAuthenticated]
-    pagination_class = FastPageNumberPagination
+    permission_classes = [AllowAny]
+    pagination_class = CursorPagination
 
     def get_queryset(self):
         first_serving = ProductServingUnit.objects.filter(

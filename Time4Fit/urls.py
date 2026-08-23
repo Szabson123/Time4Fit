0ff0a87663d7baf4b7fdb45954590ae6d29c0e2e
Szabson <szymonzaba75@gmail.com>
2026-08-23 15:51:49 +0200

    doc

--- Time4Fit/urls.py
+++ Time4Fit/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('user/', include('user.urls')),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     path('event/', include('event.urls')),

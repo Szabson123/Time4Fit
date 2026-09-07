53fb9fa2096bb27de99457683c2d85b4bc6f9a2e
Szabson <szymonzaba75@gmail.com>
2026-09-07 19:18:15 +0200

    admin fix

--- diet/admin.py
+++ diet/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from .models import ProductDescription
 
 
-admin.site.register(ProductDescription)
-
+@admin.register(ProductDescription)
+class ProductDescriptionAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['product']
